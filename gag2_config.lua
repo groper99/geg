f7c5cb098aec4bc6eea4333d7b93194d4f5a640b
groper99 <dimongtx1@gmail.com>
2026-07-12 22:49:01 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -42,7 +42,7 @@ getgenv().GAG2 = {
     CollectSeedPacks = true,
     COLLECT_PLANT_IF_MUTATED = {"Sunflower", "Venus Fly Trap", "Fire Fern", "Mushroom", "Rocket Pop",},
     SellFruitMultiplier = {
-        ["Mushroom"] = 0.86,
+        ["Mushroom"] = 0.82,
         ["Rocket Pop"] = 0.86,
     },
     BuyAuction = {
