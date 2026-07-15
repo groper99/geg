43f5e6c4a81f12b42d067490f2b94100ce20fb63
groper99 <dimongtx1@gmail.com>
2026-07-15 04:55:27 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -41,7 +41,7 @@ getgenv().GAG2 = {
      "Uncommon Sprinkler"
     },
     CollectSeedPacks = true,
-    COLLECT_PLANT_IF_MUTATED = {"Sunflower", "Venus Fly Trap", "Fire Fern", "Mushroom", "Rocket Pop","Bamboo","Pomegranate","Poison Apple",},
+    COLLECT_PLANT_IF_MUTATED = {"Sunflower", "Venus Fly Trap", "Fire Fern", "Mushroom", "Rocket Pop","Bamboo","Pomegranate","Poison Apple","Acorn","Coconut","Dragon Fruit","Mango","Banana","Grape", "Pineapple"},
     SellFruitMultiplier = {
         ["Mushroom"] = 2,
         ["Rocket Pop"] = 2,
@@ -51,6 +51,12 @@ getgenv().GAG2 = {
         ["Fire Fern"] = 3,
         ["Pomegranate"] = 3,
         ["Poison Apple"] = 3,
+        ["Acorn"] = 0.83,
+        ["Coconut"] = 0.83,
+        ["Dragon Fruit"] = 0.83,
+        ["Banana"] = 0.83,
+        ["Grape"] = 0.83,
+        ["Pineapple"] = 0.83,
     },
     BuyAuction = {
         ["Common Seed Pack"] = 25000,
