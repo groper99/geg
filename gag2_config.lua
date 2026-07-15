b28baa2836f1dd377d20c67a62a37c8b1e95f2e9
groper99 <dimongtx1@gmail.com>
2026-07-16 00:46:23 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -3,7 +3,7 @@ getgenv().GAG2 = {
     Fps = 13,
     AutoBuySeed = true,
     BuyGearShop = {
-        "Common Sprinkler", "Uncommon Sprinkler","Common Watering Can","Super Sprinkler","Super Watering Can",
+        "Common Sprinkler", "Uncommon Sprinkler","Common Watering Can","Super Sprinkler","Super Watering Can", "Trowel",
     },
     BuyGearMinSheckles = 500000,
     PlantSeeds = {
