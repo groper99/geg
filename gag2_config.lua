92480a9ffa78d5b0f9d1e279020acdbc3aa8e231
groper99 <dimongtx1@gmail.com>
2026-07-16 20:51:57 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -45,7 +45,7 @@ getgenv().GAG2 = {
 
     SellPets = {},
     UseWateringCans = { "Common Watering Can" },
-    UseSprinklers = { "Common Sprinkler", "Uncommon Sprinkler" },
+    UseSprinklers = { "Rare Sprinkler" },
 
     CollectSeedPacks = true,
 
