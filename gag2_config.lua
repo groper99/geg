504be55e32d4f5218753c599f8cd13a8c71b763b
groper99 <dimongtx1@gmail.com>
2026-07-18 18:34:11 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -101,6 +101,7 @@ getgenv().GAG2 = {
     MailReceivers = { "Prdeparitozo" },
 
     MailSendItems = {
+        ["Trowel"] = "500",
         ["Corn"] = "1200",
         ["Big Golden Dragonfly"] = "1",
         ["Huge Golden Dragonfly"] = "1",
