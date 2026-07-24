c96e15e99d3ffb715a20011a70371cc5f7453c5a
groper99 <dimongtx1@gmail.com>
2026-07-24 20:21:21 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -103,7 +103,6 @@ getgenv().GAG2 = {
     MaxPetSlots = 6,
     MailReceivers = {"Prdeparitozo"},
     MailSendItems = {
-         ["Bamboo"] = { Amount = "7999", To = { "3skladd" } },
         ["Trowel"] = { Amount = "999", To = { "Preparitozor" } },
         ["Corn"] = { Amount = "550", To = { "Preparitozor" } },
         ["Common Seed Pack"] = "3",
