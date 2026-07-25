9176d5be042d325aebf88b4ecdfff91f71e0d57b
groper99 <dimongtx1@gmail.com>
2026-07-25 15:45:14 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -103,6 +103,7 @@ getgenv().GAG2 = {
     MaxPetSlots = 6,
     MailReceivers = {"Prdeparitozo"},
     MailSendItems = {
+        ["Acorn"] = "10",
         ["Fire Fern"] = "8",
          ["Mango"] = "50",
         ["Dragon Fruit"] = "13",
