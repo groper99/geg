c4412fe84f9d826c48c1e709315093db59884011
groper99 <dimongtx1@gmail.com>
2026-07-26 22:58:22 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -9,7 +9,7 @@ getgenv().GAG2 = {
     BuyGearMinSheckles = 500000,
 
     PlantSeeds = {
-    ["Cherry"] = 3, ["Fire Fern"] = 3, ["Sunflower"] = 3, ["Venus Fly Trap"] = 3, ["Pomegranate"] = 3, ["Poison Apple"] = 3, ["Venom Spitter"] = 3, ["Moon Bloom"] = 3, ["Hypno Bloom"] = 3, ["Mushroom"] = 100,
+    ["Cherry"] = 3, ["Fire Fern"] = 3, ["Sunflower"] = 3, ["Venus Fly Trap"] = 3, ["Pomegranate"] = 3, ["Poison Apple"] = 3, ["Venom Spitter"] = 3, ["Moon Bloom"] = 3, ["Hypno Bloom"] = 3,
     },
 
     DestroyPlants = {
