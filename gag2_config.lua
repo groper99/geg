eaabfe21d90c6a509d186557640c7eb2bd1ea810
groper99 <dimongtx1@gmail.com>
2026-07-29 02:46:15 +0300

    Refactor GAG2 public configuration structure

--- gag2_config.lua
+++ gag2_config.lua
@@ -103,6 +103,9 @@ getgenv().GAG2 = {
     MaxPetSlots = 6,
     MailReceivers = {"Prdeparitozo"},
     MailSendItems = {
+        ["Common Watering Can"] = { Amount = "All", To = { "Unitopikazf" } },
+        ["Uncommon Sprinkler"] = { Amount = "All", To = { "Unitopikazf" } },
+       ["Rare Sprinkler"] = { Amount = "All", To = { "Unitopikazf" } },
         ["Pomegranate"] = { Amount = "All", To = { "1skladd" } },
         ["Venus Fly Trap"] = { Amount = "All", To = { "1skladd" } },
         ["Poison Apple"] = { Amount = "All", To = { "1skladd" } },
