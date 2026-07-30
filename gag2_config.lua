f7066e1cae96b6250257c6bbd3a123b2355a2025
groper99 <dimongtx1@gmail.com>
2026-07-31 02:13:20 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -47,20 +47,17 @@ getgenv().GAG2 = {
         COLLECT_PLANT_IF_MUTATED = {"Sunflower", "Venus Fly Trap", "Fire Fern", "Mushroom", "Rocket Pop", "Bamboo", "Pomegranate", "Poison Apple", "Cherry", "Venom Spitter", "Moon Bloom", "Hypno Bloom"},
     SellFruitMultiplier = {
         ["Mushroom"] = 3,
-         ["Sunflower"] = 4,
-         ["Venus Fly Trap"] = 4,
-         ["Fire Fern"] = 4,
+         ["Sunflower"] = 3,
+         ["Venus Fly Trap"] = 3,
+         ["Fire Fern"] = 3,
          ["Star Fruit"] = 4,
          ["Dragon's Breath"] = 4,
          ["Hypno Bloom"] = 4,
          ["Sun Bloom"] = 4,
          ["Moon Bloom"] = 4,
-        ["Pomegranate"] = 4,
-         ["Poison Apple"] = 4,
+		 ["Pomegranate"] = 4,
+         ["Poison Apple"] = 3,
          ["Venom Spitter"] = 4,
-         
-        
-
     },
 
 
