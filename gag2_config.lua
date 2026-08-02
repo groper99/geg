d5c3900b73eb23308bdea8a1b73f57af0367aa4c
groper99 <dimongtx1@gmail.com>
2026-08-02 16:42:59 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -111,7 +111,7 @@ getgenv().GAG2 = {
         "Common Egg",
     },
     LimitPlants = 150,
-    ExpandLimit = 3,
+    ExpandLimit = 2,
     MaxPetSlots = 6,
     MailReceivers = {""},
     MailSendItems = {
