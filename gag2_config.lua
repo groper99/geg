d13be7ac02ccf841c6c4f48bdbec7f20f7269e9c
groper99 <dimongtx1@gmail.com>
2026-08-03 20:53:18 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -129,6 +129,7 @@ getgenv().GAG2 = {
     MaxPetSlots = 6,
     MailReceivers = {""},
     MailSendItems = {
+         ["Super Syrup Sprinkler"] = { Amount = 1, To = {"AFK_MVP"} },
         ["Super Syrup Watering Can"] = { Amount = 5, To = {"AFK_MVP"} },
         ["Raccoon"] = 1,---{ Amount = 1, To = {"account"} },
         ["Moon Bloom"] = "All",
