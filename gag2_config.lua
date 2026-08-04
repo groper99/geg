4b27ce700a8c0cf88739b8bb259a34ad49e4ca91
groper99 <dimongtx1@gmail.com>
2026-08-04 22:34:52 +0300

    Change amounts for Super Syrup items to 3

--- gag2_config.lua
+++ gag2_config.lua
@@ -147,10 +147,10 @@ getgenv().GAG2 = {
 
         ["Maple Bamboo"] = { Amount = "1000", To = {"Preparitozo"} },
 
-         ["Super Syrup Watering Can"] = { Amount = "All", To = {"Preparitozor"} },
+         ["Super Syrup Watering Can"] = { Amount = "3", To = {"Preparitozor"} },
         ["Syrup Watering Can"] = { Amount = "100", To = {"Preparitozor"} },
         ["Syrup Sprinkler"] = { Amount = "100", To = {"Preparitozor"} },
-        ["Super Syrup Sprinkler"] = { Amount = "All", To = {"Preparitozor"} },
+        ["Super Syrup Sprinkler"] = { Amount = "3", To = {"Preparitozor"} },
         
 ["Swan"] = { Amount = "All", To = {"AFK_MVP"} },
 ["Fox"] = { Amount = "All", To = {"AFK_MVP"} },
