0616ee3ab6efc1e461ab773a8f2fdeee58b9c6fa
groper99 <dimongtx1@gmail.com>
2026-08-09 20:31:43 +0300

    Update gag2_config.lua

--- gag2_config.lua
+++ gag2_config.lua
@@ -145,6 +145,7 @@ getgenv().GAG2 = {
          ["Maple Venom Spitter"] = "All",
 
          ["Maple Mushroom"] = { Amount = "30", To = {"4skladd"} },
+       ["Maple Carrot"] = { Amount = "4500", To = {"4skladd"} },
         
          
         ["Raccoon"] = 1,---{ Amount = 1, To = {"account"} },
@@ -158,7 +159,7 @@ getgenv().GAG2 = {
         ["Maple Bamboo"] = { Amount = "2000", To = {"4skladd"} },
 
         ["Maple Sunflower"] = { Amount = "5", To = {"Gragufinicuz_2"} },
-           ["Maple Mushroom"] = { Amount = "100", To = {"Gragufinicuz_2"} },
+         
 
          ["Trowel"] = { Amount = "500", To = {"Gragufinicuz_1"} },
 
